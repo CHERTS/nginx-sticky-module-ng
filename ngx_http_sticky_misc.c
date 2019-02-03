f27d2239eae1295a1a49ea53362ae599076ae291
Mikhail Grigorev <sleuthhound@gmail.com>
2019-02-03 17:30:08 +0500

    Fix SHA_CBLOCK

--- ngx_http_sticky_misc.c
+++ ngx_http_sticky_misc.c
@@ -22,7 +22,7 @@
 #define MD5_DIGEST_LENGTH 16
 
 /* fix for new stable version 1.14.2 */
-#define SHA_CBLOCK 64
+//#define SHA_CBLOCK 64
 #define SHA_DIGEST_LENGTH 20
 
 // /* - bugfix for compiling on sles11 - needs gcc4.6 or later*/
